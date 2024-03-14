2ad0cd192fe26e820514343e9a9b0b0b46df7315
amal2608 <160566563+amal2608@users.noreply.github.com>
2024-03-14 20:18:01 +0530

    Add files via upload

---
+++ src/components/Welcomepage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Main.css';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+const Welcomepage = () => {
+  return (
+    <div className=''>
+
+      <Navbar/>
+      <Sidebar/>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 className='header'>Welcome to ACP Event Managment</h1>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      
+       <h2 className='header'>AdminDashboard</h2>
+
+    </div>
+  )
+}
+
+export default Welcomepage
